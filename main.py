feb8c18affbbd1bd45a4e48f5afe85a11cd380df
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2024-05-07 18:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from write_io import logger
 from write_io.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from write_io.pipeline.stage2_data_preprocessing.py import DataPreProcessingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
-try:
-    logger.info(f"------------- STAGE {STAGE_NAME} Started -------------")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f"------------- STAGE {STAGE_NAME} Completed -------------")
-except Exception as e:
-    logger.exception(e)
-    raise e
+STAGES = ["Data Ingestion Stage" , "Data Pre Processing Stage"]
+for stage in STAGES:
+    try:
+        logger.info(f"------------- STAGE {STAGE_NAME} Started -------------")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f"------------- STAGE {STAGE_NAME} Completed -------------")
+    except Exception as e:
+        logger.exception(e)
+        raise e
