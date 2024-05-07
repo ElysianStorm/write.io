6f4c00a7fcbc8f9bc5ded3515dccfabe083c835d
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2024-05-07 17:30:16 +0530

    Update stage2_data_preprocessing.py

--- src/write_io/pipeline/stage2_data_preprocessing.py
+++ src/write_io/pipeline/stage2_data_preprocessing.py
@@ -1,1 +1,27 @@
+from write_io.components.data_pre_processing import DataPreProcessing
+from write_io import logger
 
+STAGE_NAME = "Data Ingestion stage"
+
+class DataPreProcessingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_preprocessing_config = config.get_file_ppparms_config()
+        data_pre_processing = DataPreProcessing(config=data_pre_processing_config)
+        data_pre_processing.csv_cleanup()
+        data_pre_processing.image_cleanup()
+        data_pre_processing.reformat_csv()
+        data_pre_processing.image_normalization()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"------------- STAGE {STAGE_NAME} Started -------------")
+        obj = DataPreProcessingPipeline()
+        obj.main()
+        logger.info(f"------------- STAGE {STAGE_NAME} Completed -------------")
+    except Exception as e:
+        logger.exception(e)
+        raise e
