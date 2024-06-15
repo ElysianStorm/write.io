f2c548d3aaf2b7ba94ed1023ba105ecb53cd2ece
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2024-06-15 18:23:40 +0530

    Create prepare_model.py

---
+++ src/write_io/components/prepare_model.py
@@ -0,0 +1,59 @@
+from write_io.entity.config_entity import PrepareModelConfig
+from write_io import logger
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class PrepareBaseModel:
+    def __init__(self, config_model: PrepareModelConfig):
+        self.config_model = config_model
+        self.alphabets = config_model.alphabets
+        self.max_str_len = config_model.max_str_len # max length of input labels
+        self.num_of_characters = config_model.num_of_characters # +1 for ctc pseudo blank
+        self.num_of_timestamps = config_model.num_of_timestamps # max length of predicted labels
+
+        # Get Processed Data
+        self.config_preprocessed_data = config_preprocessed_data
+        csv_path_training = config_preprocessed_data.file_path_training
+        csv_path_validation = config_preprocessed_data.file_path_validation
+        self.train = pd.read_csv(csv_path_training)
+        self.valid = pd.read_csv(csv_path_validation)
+        self.img_train = config_preprocessed_data.image_path_training
+        self.img_valid = config_preprocessed_data.image_path_validation
+
+    
+    def process_labels(self):
+        # Process characters to convert them into numbers and add padding to set each label to same length
+    
+        # train_y, valid_y contains the true labels converted to numbers and padded with -1. The length of each label is equal to max_str_len.
+        # train_label_len, valid_label_len contains the length of each true label (without padding)
+        # train_input_len, valid_input_len contains the length of each predicted label. The length of all the predicted labels is constant i.e number of timestamps - 2.
+        # train_output, valid_output is a dummy output for ctc loss.
+        
+        # Process Labels for Training Data
+        train_size = self.config_preprocessed_data.train_size
+        valid_size = self.config_preprocessed_data.validation_size
+               
+        train_y = np.ones([train_size, self.max_str_len]) * -1
+        train_label_len = np.zeros([train_size, 1])
+        train_input_len = np.ones([train_size, 1]) * (self.num_of_timestamps-2)
+        train_output = np.zeros([train_size])
+
+        for i in range(train_size):
+            train_label_len[i] = len(self.train.loc[i, 'IDENTITY'])
+            train_y[i, 0:len(self.train.loc[i, 'IDENTITY'])]= self.label_to_num(self.train.loc[i, 'IDENTITY'])
+
+        logger.info(f"Training data processed for base model.")
+
+        # Process Labels for Validation Data
+        valid_y = np.ones([valid_size, self.max_str_len]) * -1
+        valid_label_len = np.zeros([valid_size, 1])
+        valid_input_len = np.ones([valid_size, 1]) * (self.num_of_timestamps-2)
+        valid_output = np.zeros([valid_size])
+
+        for i in range(valid_size):
+            valid_label_len[i] = len(self.valid.loc[i, 'IDENTITY'])
+            valid_y[i, 0:len(self.valid.loc[i, 'IDENTITY'])]= self.label_to_num(self.valid.loc[i, 'IDENTITY'])  
+
+        logger.info(f"Training data processed for base model.")
